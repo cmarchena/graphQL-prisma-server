294f2a72b6014b358f34ad5e8c8fcc3947b0507a
Carlos Marchena <cmarchena77@gmail.com>
2018-07-02 17:35:04 +0200

    Chapter 8 completed

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const {
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
 const resolvers = {
     Query,
     Mutation,
-    AuthPayload
+    AuthPayload,
+    Subscription,
 }
 
 
