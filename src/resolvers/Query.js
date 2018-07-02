2952e6a2562d771b430a8e90b11ef33753af3ea7
Carlos Marchena <cmarchena77@gmail.com>
2018-07-02 19:10:42 +0200

    Last Chapter up to sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,31 @@
-function feed(parent, args, context, info) {
-    return context.db.query.links({}, info)
+function info() {
+    return `This is the API of a Hackernews Clone`
 }
+
+function feed(parent, args, context, info) {
+    const where = args.filter ? {
+        OR: [{
+                url_contains: args.filter
+            },
+            {
+                description_contains: args.filter
+            },
+        ],
+    } : {}
+
+    return context.db.query.links({
+            where,
+            skip: args.skip,
+            first: args.first,
+            orderBy: args.orderBy
+        },
+        info,
+    )
+}
+
+
 
 module.exports = {
     feed,
+    info
 }
